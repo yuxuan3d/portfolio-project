1f02ce5b5a89074f551d66066b4374b4f72a828a
Accany1 <129660499+Accany1@users.noreply.github.com>
2025-04-18 08:28:45 +0800

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Experiments from './components/Experiments';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import './index.css'; // Import global styles
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     <> {/* Using Fragment to avoid extra div */}
       <Navbar />
       <main>
+        <Analytics />
         <Hero id="home"/>
         <Skills id="skills"/>
         <Projects id="projects"/>
